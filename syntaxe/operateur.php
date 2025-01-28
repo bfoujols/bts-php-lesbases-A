d1d9f9dda1fa9f7ae6a3d8af2219d2c025d0ba4d
Benoit Foujols <benoit@foujols.com>
2025-01-28 18:08:18 +0100

    Update operateur.php

---
+++ syntaxe/operateur.php
@@ -0,0 +1,22 @@
+<?php
+
+// incrementation
+$item = 0;
+
+// 1
+$item += 1;
+
+// 2
+$item = $item + 1;
+
+// 3
+$item++;
+
+echo "Incrementation de la variable \$item = " . $item . " \r";
+
+// Opération de calcul
+$nbPersonne = 10;
+$total = ((10 + 1) - $nbPersonne) * 2;
+// 2
+echo "Total du nombre de personne = " . $total;
+
